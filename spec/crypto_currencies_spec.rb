bd19feb1d852bf17b4120852840a4a1e78305b54
karimcode <karim.fathi@hec.edu>
2020-07-23 17:57:36 +0200

    Changes in game_spec

--- spec/crypto_currencies_spec.rb
+++
@@ -1,7 +0,0 @@
-require_relative '../lib/crypto_currencies'
-
-describe "the crypto_scrapper method" do
-    it "should return hash, and hash is not nil" do
-      expect(crypto_scrapper).not_to be_nil
-    end
-  end
